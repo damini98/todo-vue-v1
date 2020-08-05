51a16318f363d84d0568fee7b5225032611ee5f9
Damini Sheth <daminisheth98@gmail.com>
2020-08-05 06:51:27 +0530

    basic vue v1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <Header></Header>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/layout/Header.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -23,6 +26,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: white;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <form @submit.prevent="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add an Item">
+            <input type="submit" value="Add" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+
+//import uuid from 'uuid';
+//import uuid from "vue-uuid";
+//const { v4: uuidV4 } = require('uuid');
+
+export default {
+    name: 'AddTodo',
+    data(){
+        return {
+            title: '',
+        }
+    },
+    methods:{
+        addTodo(){
+            //e.preventDefualt();
+            //let uid = todos.length;
+            const newTodo = {
+                //id: uuidV4(),
+                title: this.title,
+                completed: false,
+            }
+            //send to parent
+            this.$emit('add-todo', newTodo);
+            this.title = '';
+        }
+    }
+}
+</script>
+
+
+
+
+<style scoped>
+form {
+    display: flex;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
